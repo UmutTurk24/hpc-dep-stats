cd619191357246e2a9d6f851894ff86e099d9e07
UmutTurk24 <umturk@davidson.edu>
2025-10-14 10:43:57 -0400

    chore: clean repo, ignore dist and add proper .gitignore

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/**/*.ts', 'src/**/*.tsx'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-unused-vars': 'warn',
+  },
+}
